1ee3782ebdc93c4a730cc2a720c9120e704f83a6
sanadsameh03 <samehsanad791@gmail.com>
2025-02-04 14:23:48 +0300

    update task3 and task3 04-02-2025 2:223

--- lib/tasks/task3.dart
+++ lib/tasks/task3.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
-class ListViewExample extends StatelessWidget {
-  ListViewExample({super.key});
+// ListViewExample..
+class Task3 extends StatelessWidget {
+  Task3({super.key});
   final products = List.generate(100, (index) => 'Product $index');
 
   @override
